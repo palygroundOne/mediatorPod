862d4d6e2230a6b69953f6aedec2ea28c2978791
yunlou1995 <lizhuo1995pro@gmail.com>
2018-11-07 14:19:38 +0800

    4

--- Pods/Target Support Files/A_Category/A_Category-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_A_Category : NSObject
-@end
-@implementation PodsDummy_A_Category
-@end
--- Pods/Target Support Files/B_Category/B_Category-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_B_Category : NSObject
-@end
-@implementation PodsDummy_B_Category
-@end
